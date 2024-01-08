be092645f867c072896010a9425c1f61c30b41fa
Tiago de Tarso <ttrgoncalves@gmail.com>
2024-01-08 11:17:05 -0300

    feat: Added lightTheme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Cadastros
+
+Projeto React + MUI + Typescript, feito por ocasião do acompanhamento do curso disponível em:
+https://www.youtube.com/playlist?list=PL29TaWXah3iaqOejItvW--TaFr9NcruyQ
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 export const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/inicio" element={<p>Pagina Inicial</p>}/>
+            <Route path="/inicio" element={<Button variant='contained' color='primary'>Teste</Button>}/>
             
             <Route path="*" element={<Navigate to="/inicio"/>}/>
         </Routes>
